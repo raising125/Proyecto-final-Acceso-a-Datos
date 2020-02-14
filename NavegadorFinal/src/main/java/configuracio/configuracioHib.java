52a38795c25c081c504bf38cfabb1eb7fbc0c738
AngelElBappo <48328589+raising125@users.noreply.github.com>
2020-02-14 17:57:53 +0100

    Clases ConfiguracioHib y MongoDB
    Se han rellenado.

--- NavegadorFinal/src/main/java/configuracio/configuracioHib.java
+++ NavegadorFinal/src/main/java/configuracio/configuracioHib.java
@@ -1,5 +1,29 @@
 package configuracio;
 
+import java.io.File;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+
 public class configuracioHib {
+	private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+        		Configuration configuration = new Configuration().configure(new File("hibernate.cfg.xml"));		//se crea la config para hibernate desde xml
+        		
+        		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
 
 }
