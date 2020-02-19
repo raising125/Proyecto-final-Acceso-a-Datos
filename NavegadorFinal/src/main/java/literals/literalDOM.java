2cf2ce8a9d55e5d838be1ffc282cbd53ca6b0d71
LucasGarau <48447004+LucasGarau@users.noreply.github.com>
2020-02-19 17:18:01 +0100

    Update literalDOM.java

--- NavegadorFinal/src/main/java/literals/literalDOM.java
+++ NavegadorFinal/src/main/java/literals/literalDOM.java
@@ -1,5 +1,28 @@
 package literals;
 
-public class literalDOM {
+import java.io.File;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import com.sun.xml.txw2.Document;
+
+public class literalDOM {
+	File file;
+	public void loadfile_XML(String filename) {
+		 file = new File(filename+".xml");
+		
+	}
+	public void parsefile_XML() {
+		try {
+			  DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			  DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			  org.w3c.dom.Document doc = dBuilder.parse(file);
+			} catch(Exception e) {
+			  e.printStackTrace();
+			}
+		
+	}
+	public void name() {
+		
+	}
 }
