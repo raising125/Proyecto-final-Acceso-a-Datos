de7161f4abfbd977bf92e2993dea88c7c2d9d9da
peremoya2000 <48447693+peremoya2000@users.noreply.github.com>
2020-02-20 20:54:58 +0100

    Create crearidi.sql

---
+++ NavegadorFinal/crearidi.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `literal` (
+  `lit_id` int(11) NOT NULL AUTO_INCREMENT,
+  `idi_cod` varchar(3) NOT NULL,
+  `lit_clau` varchar(30) NOT NULL,
+  `lit_text` varchar(300) NOT NULL,
+  PRIMARY KEY (`lit_id`),
+  UNIQUE KEY `lit_clau` (`lit_clau`,`idi_cod`),
+  KEY `idi_cod` (`idi_cod`),
+  CONSTRAINT `literal_ibfk_1` FOREIGN KEY (`idi_cod`) REFERENCES `idioma` (`idi_cod`) ON DELETE CASCADE ON UPDATE RESTRICT
+);
