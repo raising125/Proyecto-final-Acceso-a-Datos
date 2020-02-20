a5fe0a8cdc4c98074eb62273dddaa172b3b8f69b
daniel <danilaia12@hotmail.com>
2020-02-20 19:51:10 +0100

    eliminasion de configuracion

--- NavegadorFinal/src/main/java/configuracio/configuracioHib.java
+++ NavegadorFinal/src/main/java/configuracio/configuracioHib.java
@@ -1,18 +1,5 @@
 package configuracio;
 
-import java.io.File;
-
-
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-
-import entitatsHib.literal;
-import entitatsHib.configuracio;
-import entitatsHib.idioma;
-import entitatsHib.log;
-import entitatsHib.usuari;
 
 public class configuracioHib {
 
